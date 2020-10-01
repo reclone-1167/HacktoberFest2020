2a5d5fedb33f9d0a5c90c16164f8e4077b0cbcd4
Rahul kauraiya <rkauraiya1167@gmail.com>
2020-10-01 08:10:16 +0530

    bs added

--- intro.cpp
+++ intro.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 
+int bs(vector<int>arr, int key){
+	int s = 0;
+	int e = arr.size();
+	while(s <= e){
+		int mid = s + (e - s)/2;
+		if(arr[mid] == key){
+			return mid;
+		}
+		else if(arr[mid] > key){
+			s = mid + 1;
+		}
+		else e = mid - 1;
+	}
+	return -1;
+}
+
 int main(){
 	cout<<"Hello World"<<endl;
 	cout<<"Hello Rahul"<<endl; 
